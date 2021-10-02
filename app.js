feb764916d65569eb494fe6eca6fcf790ad25bc3
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-10-02 20:13:22 +0800

    Сделан плавный переход по ссылкам

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 $(function(){
 
+    /* Fixed Header */
     let header = $("#header");
     let intro = $("#intro");
-    let introH;
+    let introH = intro.innerHeight();;
     let scrollPos = $(window).scrollTop();
 
-    $(window).on("scroll load resize", function(){
+    checkScroll(scrollPos, introH)
+
+    $(window).on("scroll resize", function(){
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
-        console.log(scrollPos);
+        
+        checkScroll(scrollPos, introH)
+    });
+
+    function checkScroll(scrollPos, introH){
         if(scrollPos > introH){
             header.addClass("fixed");
         }else{
             header.removeClass("fixed")
         }
+    }
 
-    });
+    /* Smooth scroll */
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault()
 
+        let blockID = $(this).data('scroll')
+        let blockOffset = $(blockID).offset().top
+
+        $("html, body").animate({
+            scrollTop: blockOffset - 70
+        }, 700)
+    })
     
 
 });
